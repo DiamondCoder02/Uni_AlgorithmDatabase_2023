9503c5da4ccb148d05afcc10572d6114f2711234
DiamondPRO02 <kovacsdanielmark@gmail.com>
2023-02-07 09:28:09 +0100

    Lesson done

--- lesson1.cpp
+++ lesson1.cpp
@@ -1,37 +1,48 @@
 // DiamondCoder 2023.02.07
 #include <iostream>
+#include <stdio.h>
+#include <cmath>
 using namespace std;
+//On old system: setlocale(LC_ALL, "hun");
+
 
 const int N = 10;
-int ids[N] = { 9, 10,18,23,32,41,45,60,72 };
+int ids[N] = { 9,10,18,23,32,41,45,60,72 };
 
 int main() {
 	int search = 45;
+	int steps = 0;
 	
 	// Linear Searching
 	int result = -1; // -1 means not found
 
 	for (int i = 0; i < N; i++) {
+		steps++;
 		if (ids[i] == search) {
 			result = i;
+			break;
+		}
+		else if (ids[i] > search) {
 			break;
 		}
 	}
 	
 	if (result != -1) {
-		cout << "Found at " << result << endl;
+		cout << "Found at " << result << ", steps taken: " << steps << endl;
 	}
 	else {
-		cout << "Not found" << endl;
+		cout << "Not found" << ", steps taken: " << steps << endl;
 	}
 
 	// Bi Searching
 	result = -1;
+	steps = 0;
 	int top = N - 1;
 	int bottom = 0;
 	int middle;
 	
 	while (top >= bottom) {
+		steps++;
 		middle = (top + bottom) / 2;
 		if (ids[middle] == search) {
 			result = middle;
@@ -46,9 +57,9 @@ int main() {
 	}
 
 	if (result != -1) {
-		cout << "Found at " << result << endl;
+		cout << "Found at " << result << ", steps taken: " << steps << endl;
 	}
 	else {
-		cout << "Not found" << endl;
+		cout << "Not found" << ", steps taken: " << steps << endl;
 	}
 }
